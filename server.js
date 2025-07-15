294bb35c19b626bacb6937478cc2ba4fd3f42406
Sidhhrana <156512229+Sidhhrana@users.noreply.github.com>
2025-07-15 20:27:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -353,7 +353,4 @@ app.get('/', (req, res) => {
   res.send('✅ Server is running!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
